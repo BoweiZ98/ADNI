942628c0b2b792290ed9642823c934472e9eb365
BoweiZ98 <zbwzhang98@gmail.com>
2024-02-04 20:52:15 -0800

    preliminary data wrangling

--- 1_Data_Wrangling.R
+++ 1_Data_Wrangling.R
@@ -3,8 +3,42 @@ install.packages("./ADNIMERGE_0.0.1.tar.gz", repos = NULL, type = "source")
 library(ADNIMERGE)
 data(adnimerge)
 
-# Summary
-remotes::install_github("dcomtois/summarytools", build_vignettes = TRUE)
-library(summarytools)
-dfSummary(adnimerge)
+# Data Manipulation 
+library(tidyverse)
+mci_id = adnimerge %>%
+  filter(DX.bl %in% c("EMCI", "LMCI")) %>%
+  select(RID) %>%
+  unique() %>%
+  pull(RID)
 
+
+converter_id = adnimerge %>%
+  filter(DX.bl %in% c("EMCI", "LMCI") & DX == "Dementia") %>%
+  select(RID) %>%
+  unique() %>%
+  pull(RID)
+
+non_converter_id = setdiff(mci_id, converter_id)
+
+adni_mci = adnimerge %>%
+  filter(RID %in% mci_id & VISCODE == "bl") %>% # baseline visit
+  mutate(converter = case_when(
+    RID %in% converter_id ~ 1,
+    RID %in% non_converter_id ~ 0
+  )) %>%
+  select(
+    # Sociodemographic 
+    PTGENDER, AGE, PTEDUCAT, PTMARRY,
+    # subtypes of MCI
+    DX.bl,
+    # clinical scales
+    CDRSB.bl, CDRSB.bl, FAQ.bl,
+    # Neurosychological tests
+    MMSE.bl, ADAS11.bl, ADAS13.bl,ADASQ4.bl, 
+    RAVLT.immediate.bl, RAVLT.learning.bl, RAVLT.forgetting.bl,
+    RAVLT.perc.forgetting.bl, LDELTOTAL.bl, TRABSCOR.bl,
+    # outcome
+    converter
+  )
+
+gtsummary::tbl_summary(adni_mci, b = converter)   
